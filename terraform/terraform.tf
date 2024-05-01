4786cf650b5240b08e0119772f84d0ddecfaa100
Ben James Dodwell <61091068+ben-james-dodwell@users.noreply.github.com>
2024-05-01 16:12:37 +0100

    Fix format

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket          = "cv-benjamesdodwell-com-terraform"
-    key             = "backend/terraform.tfstate"
-    region          = "eu-west-2"
-    dynamodb_table  = "cv-benjamesdodwell-com-terraform"
+    bucket         = "cv-benjamesdodwell-com-terraform"
+    key            = "backend/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "cv-benjamesdodwell-com-terraform"
   }
 
   required_providers {
